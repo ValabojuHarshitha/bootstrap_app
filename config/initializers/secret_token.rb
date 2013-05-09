9051ac06dc80385f187c6b09b993ffd7656a96f3
khamar.md@gmail.com <khamar@jupitar.(none)>
2013-05-09 15:00:54 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'adb557d39a87fb30e4130f7b3b242e479c44cc749896a870b9b3e95858f66f35c589ee86c00f128aa918e53a6f52dec1ef21489af065774f7ccf6a62e82f2c47'
